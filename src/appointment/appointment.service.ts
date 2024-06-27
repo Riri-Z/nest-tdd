178e06cba88d2deecdc54104322ff1844612928f
Riri-Z <riri.zamor@gmail.com>
2024-06-27 13:37:54 +0200

    Fix typo and add guard

--- src/appointment/appointment.service.ts
+++ src/appointment/appointment.service.ts
@@ -10,29 +10,34 @@ interface AppointmentInput {
 @Injectable()
 export class AppointmentService {
   // we want the test to pass, so we don't give fully tape parameters hence any type
-  public scheduleAppointment(appointementData: AppointmentInput): Appointment {
-    if (appointementData.endTime <= appointementData.startTime) {
+  public scheduleAppointment(appointmentData: AppointmentInput): Appointment {
+    if (appointmentData.endTime <= appointmentData.startTime) {
       throw new Error("appointment's end time should be after start time");
     }
 
-    if (this.endTimeIsInTheNextDay(appointementData)) {
-      throw new Error("appointment's end time should be in same day");
+    if (this.endTimeIsInTheNextDay(appointmentData)) {
+      throw new Error("appointment's end time should be in same day same months and same years");
     }
     return {
-      ...appointementData,
+      ...appointmentData,
       confirmed: false,
     };
   }
 
-  private endTimeIsInTheNextDay(appointementData: AppointmentInput) {
+  private endTimeIsInTheNextDay(appointmentData: AppointmentInput) {
     const differentDays =
-      appointementData.endTime.getUTCDate() !==
-      appointementData.startTime.getUTCDate();
+      appointmentData.endTime.getUTCDate() !==
+      appointmentData.startTime.getUTCDate();
 
     const differentMonths =
-      appointementData.endTime.getUTCMonth() !==
-      appointementData.startTime.getUTCMonth();
+      appointmentData.endTime.getUTCMonth() !==
+      appointmentData.startTime.getUTCMonth();
 
-    return differentDays || differentMonths;
+        // Now we also check for years
+  const differentYears =
+  appointmentData.endTime.getUTCFullYear() !==
+  appointmentData.startTime.getUTCFullYear();
+
+    return differentDays || differentMonths || differentYears;
   }
 }
