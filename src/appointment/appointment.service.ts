9b7a4885f4505adfc804c8665f1387c013de5885
Riri-Z <riri.zamor@gmail.com>
2024-06-27 12:55:06 +0200

    Refactor to clean code and remove any type

--- src/appointment/appointment.service.ts
+++ src/appointment/appointment.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Appointment } from './Appointment.model';
+
+interface AppointmentInput {
+  patientId: number;
+  startTime: Date;
+  endTime: Date;
+}
 
 @Injectable()
 export class AppointmentService {
-
   // we want the test to pass, so we don't give fully tape parameters hence any type
-  public scheduleAppointment(appointementData: Record<string, any>) {
+  public scheduleAppointment(appointementData: AppointmentInput): Appointment {
     return {
       ...appointementData,
       confirmed: false,
