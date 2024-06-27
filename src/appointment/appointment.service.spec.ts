99bcba6d74bb0975e7603a2a270c130b74d5b8bf
Riri-Z <riri.zamor@gmail.com>
2024-06-27 13:21:41 +0200

    add test  : should throw an error when end time is in the next day
    Update model to add guard

--- src/appointment/appointment.service.spec.ts
+++ src/appointment/appointment.service.spec.ts
@@ -1,6 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppointmentService } from './appointment.service';
 
+/*
+TEST CASES :
+-An unconfirmed schedule should be created on success
+-The end time should not be before the start time
+-The end time should be after the start time
+-An appointment start and end time should be within the same day
+-The patientId should be validated?
+
+*/
 describe('AppointmentService', () => {
   let service: AppointmentService;
 
@@ -18,7 +27,7 @@ describe('AppointmentService', () => {
 
   it('should schedule an  unconfirmed appointment for a user on success', () => {
     const startTime = new Date('2024-01-01T11:00:00Z');
-    const endTime = new Date('2024-01-01T11:00:00Z');
+    const endTime = new Date('2024-01-01T12:00:00Z');
 
     const newAppointment = service.scheduleAppointment({
       patientId: 1,
@@ -36,7 +45,7 @@ describe('AppointmentService', () => {
 
   it('should throw an error when end time is before start time', () => {
     const startTime = new Date('2024-01-01T11:00:00Z');
-    const endTime = new Date('2023-01-01T09:00:00Z');
+    const endTime = new Date('2024-01-01T09:00:00Z');
 
     // we need wrap function to watch if exception is detected
     expect(() => {
@@ -46,5 +55,31 @@ describe('AppointmentService', () => {
         endTime,
       });
     }).toThrow("appointment's end time should be after start time");
+  });
+
+  it('should throw an error when end time is equal to start time', () => {
+    const startTime = new Date('2024-01-01T11:00:00Z');
+    const endTime = startTime;
+
+    // we need wrap function to watch if exception is detected
+    expect(() => {
+      service.scheduleAppointment({
+        patientId: 1,
+        startTime,
+        endTime,
+      });
+    }).toThrow("appointment's end time should be after start time");
+  });
+  it('should throw an error when end time is in the next day', () => {
+    const startTime = new Date('2024-01-01T11:00:00Z');
+    const endTime = new Date('2024-01-02T11:00:00Z');
+
+    expect(() => {
+      service.scheduleAppointment({
+        patientId: 1,
+        startTime,
+        endTime,
+      });
+    }).toThrow("appointment's end time should be in same day");
   });
 });
--- src/appointment/appointment.service.ts
+++ src/appointment/appointment.service.ts
@@ -11,11 +11,15 @@ interface AppointmentInput {
 export class AppointmentService {
   // we want the test to pass, so we don't give fully tape parameters hence any type
   public scheduleAppointment(appointementData: AppointmentInput): Appointment {
-
-    if(appointementData.endTime < appointementData.startTime) {
+    if (appointementData.endTime <= appointementData.startTime) {
       throw new Error("appointment's end time should be after start time");
     }
 
+    if (
+      appointementData.endTime.getDay() !== appointementData.startTime.getDay()
+    ) {
+      throw new Error("appointment's end time should be in same day");
+    }
     return {
       ...appointementData,
       confirmed: false,
